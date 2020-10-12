f502a9b43c65bc557049ae1c865d2fff5afd9bfe
Rahul <reswar@uoguelph.ca>
2020-10-12 10:48:41 -0400

    tailwind css boiler plate

---
+++ src/spot-assist-ui/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      display: ['Mulish', 'sans-serif'],
+      body: ['Mulish', 'sans-serif']
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
